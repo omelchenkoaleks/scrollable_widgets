334630b54503d1c251ad20671d7a46a9aab34cb9
omelchenkoaleks <omelchenkoaleks@gmail.com>
2022-01-03 13:48:37 +0200

    How do you display your UI with an asynchronous task?

---
+++ lib/screens/explore_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../components/components.dart';
+import '../models/models.dart';
+import '../api/mock_fooderlich_service.dart';
+
+class ExploreScreen extends StatelessWidget {
+  final mockService = MockFooderlichService();
+
+  ExploreScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: mockService.getExploreData(),
+      builder: (context, AsyncSnapshot<ExploreData> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          final recipes = snapshot.data?.todayRecipes ?? [];
+          return Center(
+            child: Container(
+              child: const Text('Show TodayRecipeListView'),
+            ),
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+}
