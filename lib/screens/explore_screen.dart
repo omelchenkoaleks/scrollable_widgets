cb80df4e44b39300b4b8c386ff4b3a83baa1e83b
omelchenkoaleks <omelchenkoaleks@gmail.com>
2022-01-04 08:28:20 +0200

    adding the nested ListView

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -15,12 +15,20 @@ class ExploreScreen extends StatelessWidget {
       future: mockService.getExploreData(),
       builder: (context, AsyncSnapshot<ExploreData> snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
-          final recipes = snapshot.data?.todayRecipes ?? [];
-          return TodayRecipeListView(recipes: recipes);
-        } else {
-          return const Center(
-            child: CircularProgressIndicator(),
+          return ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
+              const SizedBox(height: 16),
+              Container(
+                height: 400,
+                color: Colors.green,
+              ),
+            ],
           );
+        } else {
+          // 10
+          return const Center(child: CircularProgressIndicator());
         }
       },
     );
