86961f93d60263cd096ec45704932adc542e5d20
omelchenkoaleks <omelchenkoaleks@gmail.com>
2022-01-05 11:41:56 +0200

    building a quantity widget with clider

--- lib/screens/grocery_item_screen.dart
+++ lib/screens/grocery_item_screen.dart
@@ -92,7 +92,8 @@ class _GroceryItemScreenState extends State<GroceryItemScreen> {
             buildTimeField(context),
             const SizedBox(height: 10.0),
             buildColorPicker(context),
-            // TODO 18: Add slider
+            const SizedBox(height: 10.0),
+            buildQuantityField(),
             // TODO: 19: Add Grocery Tile
           ],
         ),
@@ -294,6 +295,42 @@ class _GroceryItemScreenState extends State<GroceryItemScreen> {
     );
   }
 
-  // TODO: Add buildQuantityField()
-
+  Widget buildQuantityField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.baseline,
+          textBaseline: TextBaseline.alphabetic,
+          children: [
+            Text(
+              'Quantity',
+              style: GoogleFonts.lato(fontSize: 28.0),
+            ),
+            const SizedBox(width: 16.0),
+            Text(
+              _currentSliderValue.toInt().toString(),
+              style: GoogleFonts.lato(fontSize: 18.0),
+            ),
+          ],
+        ),
+        Slider(
+          inactiveColor: _currentColor.withOpacity(0.5),
+          activeColor: _currentColor,
+          value: _currentSliderValue.toDouble(),
+          min: 0.0,
+          max: 100.0,
+          divisions: 100,
+          label: _currentSliderValue.toInt().toString(),
+          onChanged: (double value) {
+            setState(
+              () {
+                _currentSliderValue = value.toInt();
+              },
+            );
+          },
+        ),
+      ],
+    );
+  }
 }
