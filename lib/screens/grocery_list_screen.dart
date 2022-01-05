80824b3201637e2e484b0364c05f8495f74e5c5c
omelchenkoaleks <omelchenkoaleks@gmail.com>
2022-01-05 17:00:12 +0200

    dismissing items with a swipe

--- lib/screens/grocery_list_screen.dart
+++ lib/screens/grocery_list_screen.dart
@@ -20,31 +20,50 @@ class GroceryListScreen extends StatelessWidget {
         itemCount: groceryItems.length,
         itemBuilder: (context, index) {
           final item = groceryItems[index];
-          // TODO 28: Wrap in a Dismissable
-          return InkWell(
-            child: GroceryTile(
-                key: Key(item.id),
-                item: item,
-                onComplete: (change) {
-                  if (change != null) {
-                    manager.completeItem(index, change);
-                  }
-                }),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => GroceryItemScreen(
-                    originalItem: item,
-                    onUpdate: (item) {
-                      manager.updateItem(item, index);
-                      Navigator.pop(context);
-                    },
-                    onCreate: (item) {},
-                  ),
-                ),
-              );
+          return Dismissible(
+            // The dismissible widget includes a Key. Flutter needs this to find and remove the right element in the tree.
+            key: Key(item.id),
+            // Sets the direction the user can swipe to dismiss.
+            direction: DismissDirection.endToStart,
+            // Selects the background widget to display behind the widget you’re swiping. In this case, the background widget is red with a white trash can Icon aligned in the center and to the right of the Container.
+            background: Container(
+                color: Colors.red,
+                alignment: Alignment.centerRight,
+                child: const Icon(Icons.delete_forever,
+                    color: Colors.white, size: 50.0)),
+            // onDismissed lets you know when the user swiped away a GroceryTile.
+            onDismissed: (direction) {
+              // Lets the grocery manager handle deleting the item, given an index.
+              manager.deleteItem(index);
+              // Shows a snack bar widget to let the user know which item they deleted.
+              ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('${item.name} dismissed')));
             },
+            child: InkWell(
+              child: GroceryTile(
+                  key: Key(item.id),
+                  item: item,
+                  onComplete: (change) {
+                    if (change != null) {
+                      manager.completeItem(index, change);
+                    }
+                  }),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => GroceryItemScreen(
+                      originalItem: item,
+                      onUpdate: (item) {
+                        manager.updateItem(item, index);
+                        Navigator.pop(context);
+                      },
+                      onCreate: (item) {},
+                    ),
+                  ),
+                );
+              },
+            ),
           );
         },
         separatorBuilder: (context, index) {
