11a2cc83b7d8182a280a0e56f096679ff61b8ffb
omelchenkoaleks <omelchenkoaleks@gmail.com>
2022-01-05 12:08:51 +0200

    Change some of your choices, such as the name or quantity, and see the tile update automatically.

--- lib/components/grocery_tile.dart
+++ lib/components/grocery_tile.dart
@@ -24,8 +24,45 @@ class GroceryTile extends StatelessWidget {
     // TODO 21: Change this Widget
     return Container(
       height: 100.0,
-      // TODO 20: Replace this color
-      color: Colors.red,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Container(width: 5.0, color: item.color),
+              const SizedBox(width: 16.0),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    item.name,
+                    style: GoogleFonts.lato(
+                      decoration: textDecoration,
+                      fontSize: 21.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 4.0),
+                  buildDate(),
+                  const SizedBox(height: 4.0),
+                  buildImportance(),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Text(
+                item.quantity.toString(),
+                style: GoogleFonts.lato(
+                    decoration: textDecoration, fontSize: 21.0),
+              ),
+              buildCheckbox(),
+            ],
+          ),
+        ],
+      ),
     );
   }
 
